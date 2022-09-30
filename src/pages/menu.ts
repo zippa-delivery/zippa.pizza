4510f3c5a51b18009263f0ad5db870d17b44076c
Alex Sandri <me@alexsandri.com>
2022-09-30 19:54:41 +0200

    use transparent header on mobile too

--- src/pages/menu.ts
+++ src/pages/menu.ts
@@ -1,6 +1,7 @@
 export const setupMenu = () => {
-  const navToggle = document.getElementById("nav-toggle")!;
-  const navContainer = document.getElementById("nav-container")!;
+  const header = document.querySelector('header')!;
+  const navToggle = document.getElementById('nav-toggle')!;
+  const navContainer = document.getElementById('nav-container')!;
 
   const toggle = () => {
     navToggle.setAttribute(
@@ -8,12 +9,13 @@ export const setupMenu = () => {
       navContainer.hasAttribute('data-show') ? 'Apri menu' : 'Chiudi menu',
     );
 
-    navContainer.toggleAttribute("data-show");
+    header.toggleAttribute('data-nav-visible');
+    navContainer.toggleAttribute('data-show');
   };
 
-  navToggle.addEventListener("click", toggle);
+  navToggle.addEventListener('click', toggle);
 
-  navContainer.addEventListener("click", e => {
+  navContainer.addEventListener('click', e => {
     const nav = document.querySelector('#nav-container > nav')!;
 
     if (nav.contains(e.target as HTMLElement)) {
